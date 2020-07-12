84723eeded367006a4ed33912fbb837eb5d5f257
Lucas Angelo <lcs2001_@hotmail.com>
2020-07-12 17:47:46 -0300

    projeto, detectar hora do sistema

---
+++ HoraDoSistema/src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,13 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date relogio = new Date(); // new cria uma novo objeto, nesse utilizando a biblioteca Date.
+        System.out.print("A hora do sistema é: ");
+        System.out.println(relogio.toString());
+    }
+    
+}
