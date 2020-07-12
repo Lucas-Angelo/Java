d84b4758b3786ea180f2615066044113727d38fa
Lucas Angelo <lcs2001_@hotmail.com>
2020-07-12 17:31:22 -0300

    hello world em java + comentários

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package primeiroprograma; //Somente letra minúscula, é nome de pacote
+
+public class PrimeiroPrograma { // Primeira letra maiúscula, é classe.
+    // Java é case sensitive, então utiliza-se cammelCase.
+    
+    public static void main(String[] args) { // Escrever psvm + TAB.
+        System.out.println("Hello World!"); // Escrever: sout + TAB.
+        //Existem print e println(com quebra de linha).
+    }
+    
+    /* Se a primeira letra for minúcula e tiver cammel case tipo: minhaVariavel, 
+    pode ser variável, atributo ou método.*/
+    
+    //Todas as letras maiúsculas, é uma constante.
+    
+    
+}
