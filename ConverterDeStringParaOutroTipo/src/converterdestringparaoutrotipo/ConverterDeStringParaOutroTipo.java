6517f54c108cc704e40ffe0e1a676506cea9b021
Lucas Angelo <lcs2001_@hotmail.com>
2020-07-17 17:01:32 -0300

    Converter tipo String para outros tipos

---
+++ ConverterDeStringParaOutroTipo/src/converterdestringparaoutrotipo/ConverterDeStringParaOutroTipo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converterdestringparaoutrotipo;
+
+/**
+ *
+ * @author Lucas Ângelo github.com/Lucas-Angelo
+ */
+public class ConverterDeStringParaOutroTipo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        System.out.println(idade);
+    }
+    
+}
