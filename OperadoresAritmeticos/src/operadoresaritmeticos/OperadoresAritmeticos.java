364600fbe1bdaff121a5965c3ff158115d36899e
Lucas Angelo <lcs2001_@hotmail.com>
2020-07-18 17:08:57 -0300

    operadores unarios e aritmeticos

--- OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -19,6 +19,20 @@ public class OperadoresAritmeticos {
         int n2 = 5;
         float m = (n1+n2)/2;
         System.out.printf("A media é igual a %.2f \n", m);
+        
+        int soma = 5;
+        soma++;
+        System.out.printf("A soma é %d \n", soma);
+        
+        //Pré incremento
+        int numPre = 5;
+        int somatorioPre = 10 + ++numPre;
+        System.out.printf("O somatório pre é %d. E numPre e: %d \n", somatorioPre, numPre);
+        
+        //Pós incremento, só faz após efetuar o cálculo
+        int numPos = 5;
+        int somatorioPos = 10 + numPos++;
+        System.out.printf("O somatório pos é %d. E numPos e: %d \n", somatorioPos, numPos);
     }
     
 }
