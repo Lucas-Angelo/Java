b84a42f7824207eb4c0852f1e52a4a8f42a1ce9c
Lucas Angelo <lcs2001_@hotmail.com>
2020-07-18 16:33:38 -0300

    operadores aritimeticos em java

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author Lucas Ângelo github.com/Lucas-Angelo
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1+n2)/2;
+        System.out.printf("A media é igual a %.2f \n", m);
+    }
+    
+}
